66a468b31a3f2d7cbcc1f84db14a3d13851c4292
gbarshad <47452349+gbarshad@users.noreply.github.com>
2023-01-12 19:01:05 -0500

    Add files via upload

--- Contact_normalization_by_local_decay/Plotting_obs_over_exp_distribution_by_pair_type.py
+++ Contact_normalization_by_local_decay/Plotting_obs_over_exp_distribution_by_pair_type.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
 import sys
 '''
 1 - ContactCaller_microC_output_W_functional_nonfunctional_and_oter_pair_assignments.txt
